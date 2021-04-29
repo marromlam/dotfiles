221f2fcfccca4d0daf3f6d3dc7ba5304aaa03fd0
Marcos Romero <marromlam@gmail.com>
2021-04-29 16:33:55 +0200

    add fzf config

---
+++ files/.fzf.zsh
@@ -0,0 +1,13 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home3/marcos.romero/.masterbrew/opt/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}$HOMEBREW/opt/fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "$HOMEBREW/opt/fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "$HOMEBREW/opt/fzf/shell/key-bindings.zsh"
