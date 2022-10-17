6848dd9e10fa4ad042a35da320367ff233951acc
Marcos Romero Lamas <mromerol@cern.ch>
2022-10-17 22:22:16 +0000

    feat: update ignored

--- zsh/ufunctions.sh
+++ zsh/ufunctions.sh
@@ -11,46 +11,10 @@ function version { echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4
 # https://www.topbug.net/blog/2016/10/11/speed-test-check-the-existence-of-a-command-in-bash-and-zsh/
 exists() { (( $+commands[$1] )); }
 
-
-function blindworm() {
+function vibora() {
   source $HOME/conda3/bin/activate
   conda activate $1
 }
-
-
-# ssh functions {{{
-# Try to create a custom ssh function with socket for the clipboard and some
-# tmux things. I thik they wont work but...
-
-function sshx (){
-  ssh -t $1 "export IS_TMUX=${IS_TMUX}; zsh"
-}
-
-#_dt_term_socket_ssh() {
-#    ssh -oControlPath=$1 -O exit DUMMY_HOST
-#}
-#function sshx {
-#  #local t=$(mktemp -u --tmpdir=$HOME ssh.sock.XXXXXXXXXX)
-#  local t=$(mktemp -u -d "/Users/marcos/ssh.sock.XXXXXXXXXX")
-#  local f="~/clip"
-#  ssh -f -oControlMaster=yes -oControlPath=$t $@ tail\ -f\ /dev/null || return 1
-#  ssh -S$t DUMMY_HOST "bash -c 'if ! [ -p $f ]; then mkfifo $f; fi'" \
-#      || { _dt_term_socket_ssh $t; return 1; }
-#  (
-#  set -e
-#  set -o pipefail
-#  while [ 1 ]; do
-#      ssh -S$t DUMMY_HOST "cat $f" | xclip -selection clipboard
-#  done &
-#  )
-#  ssh -S$t DUMMY_HOST \
-#      || { _dt_term_socket_ssh $t; return 1; }
-#  ssh -S$t DUMMY_HOST "rm $f"
-#  _dt_term_socket_ssh $t
-#}
-
-# }}}
-
 
 function get_machine() {
   unameOut="$(uname -s)"
@@ -79,4 +43,4 @@ alias kill_jupyter="kill $(netstat -tulpn 2>&1 | pgrep jupyter)"
 
 # }}}
 
-# vim: fdm=marker ft=zsh
+# vim: fdm=marker ft=bash
