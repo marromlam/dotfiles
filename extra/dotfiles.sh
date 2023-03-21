4d2b2f9b3168c711df752f17ffdeddcd3d3c9a52
Marcos Romero Lamas <mromerol@cern.ch>
2023-03-21 03:01:08 +0100

    feat: add dotfiles installer

---
+++ extra/dotfiles.sh
@@ -0,0 +1,28 @@
+echo "================================================================================"
+echo "Clone dotfiles"
+echo "--------------------------------------------------------------------------------";
+
+set -e
+
+export DOTFILES="$HOME/.dotfiles"
+
+if test "$1" = "-f"; then
+  rm -rf $DOTFILES
+fi
+
+if [ -f "$DOTFILES" ]; then
+  echo "Dotfiles have already been cloned into the home dir"
+  git pull
+else
+  echo "Cloning dotfiles"
+  git clone git@github.com:marromlam/dotfiles.git $DOTFILES
+fi
+
+cd "$DOTFILES" || "Didn't cd into dotfiles this will be bad :("
+git submodule update --init --recursive
+
+exit 0
+
+echo "--------------------------------------------------------------------------------";
+echo "Done "
+echo "================================================================================"
--- install.sh
+++ install.sh
@@ -8,10 +8,14 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 # machine type
 export MACHINEOS=`$HOME/.dotfiles/scripts/machine.sh`
 
-# Set OS-dependent stuff
+# clone temp dir
+curl --create-dirs -O --output-dir ${HOME}/tmp/ https://raw.githubusercontent.com/marromlam/dotfiles/main/homebrew/linuxbrew.sh
+curl --create-dirs -O --output-dir ${HOME}/tmp/ https://raw.githubusercontent.com/marromlam/dotfiles/main/homebrew/homebrew.sh
+curl --create-dirs -O --output-dir ${HOME}/tmp/ https://raw.githubusercontent.com/marromlam/dotfiles/main/extra/keys.sh
+curl --create-dirs -O --output-dir ${HOME}/tmp/ https://raw.githubusercontent.com/marromlam/dotfiles/main/extra/dotfiles.sh
+
 if [[ "$MACHINEOS" == "Mac" ]]; then
   rm -rf ${HOME}/tmp/homebrew.sh
-  curl --create-dirs -O --output-dir ${HOME}/tmp/ https://raw.githubusercontent.com/marromlam/dotfiles/main/homebrew/linuxbrew.sh
   bash ${HOME}/tmp/linuxbrew.sh
 else
   # linuxbrew path
@@ -19,5 +23,7 @@ else
 fi
 
 # install private dotfiles
-curl --create-dirs -O --output-dir ${HOME}/tmp/ https://raw.githubusercontent.com/marromlam/dotfiles/main/homebrew/keys.sh
-bash ${HOME}/tmp/keys.sh
+printf " \n\n"
+bash ${HOME}/tmp/keys.sh -f
+
+
