7408ea4d899ba371bd4fcac705ecaa3ca2f9dc35
Marcos Romero Lamas <41004396+marromlam@users.noreply.github.com>
2023-03-23 18:03:23 +0100

    fix: skip keyboard mappings in linux

--- extra/keyboard.sh
+++ extra/keyboard.sh
@@ -6,20 +6,20 @@ echo "--------------------------------------------------------------------------
 
 set -e
 
-echo "Remap caps-lock to control for all Keyboards"
-sudo cp com.ldaws.KeyMapsGeneral.plist ~/Library/LaunchAgents 
-sudo launchctl unload -w ~/Library/LaunchAgents/com.ldaws.KeyMapsGeneral.plist &> /dev/null
-sudo launchctl load -w ~/Library/LaunchAgents/com.ldaws.KeyMapsGeneral.plist &> /dev/null
+export MACHINEOS=`$HOME/.dotfiles/scripts/machine.sh`
 
-
-# here I will remap caps-lock to control for Ducky
-# (I no longer have a Ducky keyboard, so commented out)
-
-
-# ensure media keys will work
-echo "Ensure media keys will work..."
-sudo launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist &> /dev/null
-sudo launchctl load -w /System/Library/LaunchAgents/com.apple.rcd.plist &> /dev/null
+if [[ "$MACHINEOS" == "Mac" ]]; then
+  echo "Remap caps-lock to control for all Keyboards"
+  sudo cp com.ldaws.KeyMapsGeneral.plist ~/Library/LaunchAgents 
+  sudo launchctl unload -w ~/Library/LaunchAgents/com.ldaws.KeyMapsGeneral.plist &> /dev/null
+  sudo launchctl load -w ~/Library/LaunchAgents/com.ldaws.KeyMapsGeneral.plist &> /dev/null
+  # ensure media keys will work
+  echo "Ensure media keys will work..."
+  sudo launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist &> /dev/null
+  sudo launchctl load -w /System/Library/LaunchAgents/com.apple.rcd.plist &> /dev/null
+else
+  echo "Nothing to do in linux"
+fi
 
 echo "--------------------------------------------------------------------------------";
 echo "Done "
