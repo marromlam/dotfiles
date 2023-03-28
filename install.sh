7da537f49131779fd77163677d10f4d00c90760c
Marcos Romero Lamas <41004396+marromlam@users.noreply.github.com>
2023-03-28 22:41:46 +0200

    fix: install.sh homebrew

--- install.sh
+++ install.sh
@@ -16,11 +16,12 @@ curl --create-dirs -O --output-dir ${HOME}/tmp/ https://raw.githubusercontent.co
 curl --create-dirs -O --output-dir ${HOME}/tmp/ https://raw.githubusercontent.com/marromlam/dotfiles/main/extra/dotfiles.sh
 
 if [[ "$MACHINEOS" == "Mac" ]]; then
-  rm -rf ${HOME}/tmp/homebrew.sh
-  bash ${HOME}/tmp/linuxbrew.sh
+  # rm -rf ${HOME}/tmp/homebrew.sh
+  bash ${HOME}/tmp/homebrew.sh
 else
   # linuxbrew path
   export HOMEBREW_PREFIX="$HOME/.linuxbrew"
+  bash ${HOME}/tmp/linuxbrew.sh
 fi
 
 # install private dotfiles
