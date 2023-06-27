4454504c1a1236e125901f3469d04c458d83eda1
Marcos Romero Lamas <marromlam@gmail.com>
2023-06-27 18:50:36 +0200

    feat: add fixed to dock

---
+++ extra/dock_config.sh
@@ -0,0 +1,16 @@
+dockutil -r Safari
+dockutil -r Launchpad
+dockutil -r Messages
+dockutil -r Maps
+dockutil -r Photos
+dockutil -r FaceTime
+dockutil -r Contacts
+dockutil -r Reminders
+dockutil -r Notes
+dockutil -r Freeform
+dockutil -r TV
+dockutil -r 'App Store'
+dockutil --add '/Applications/Spotify.app' --position 3
+dockutil --add '/Applications/kitty.app' --position 4
+dockutil --add '/Applications/Orion.app' --position 2
+dockutil --add '/Applications/Microsoft Teams.app' --position 5
