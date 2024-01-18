c06a64ba9e4383765f7c4ab581ff525d5cbef0f8
Marcos Romero Lamas <marromlam@gmail.com>
2024-01-18 16:38:45 +0100

    feat: update to handle x64-wsl

--- install.sh
+++ install.sh
@@ -37,13 +37,27 @@ curl -o ${HOME}/tmp/homebrew.sh $REPO_URL/$REPO_BRANCH/homebrew/homebrew.sh
 curl -o ${HOME}/tmp/keys.sh $REPO_URL/$REPO_BRANCH/extra/keys.sh
 curl -o ${HOME}/tmp/dotfiles.sh $REPO_URL/$REPO_BRANCH/extra/dotfiles.sh
 curl -o ${HOME}/tmp/reload_shell $REPO_URL/$REPO_BRANCH/scripts/reload_shell
+curl -o ${HOME}/tmp/preflight_wsl.sh $REPO_URL/$REPO_BRANCH/preflight_wsl.sh
 
-if [[ "$MACHINEOS" == "Mac" ]]; then
-	echo "Installing homebrew on macOS (forced=$0)"
-	bash ${HOME}/tmp/homebrew.sh $0
-else
+# if [[ "$MACHINEOS" == "Mac" ]]; then
+# 	echo "Installing homebrew on macOS (forced=$0)"
+# 	bash ${HOME}/tmp/homebrew.sh $0
+# else
+# 	echo "Installing homebrew on Linux (forced=$0)"
+# 	export HOMEBREW_PREFIX="$HOME/.linuxbrew"
+# 	bash ${HOME}/tmp/linuxbrew.sh $0
+# fi
+
+export MACHINE=$(cat $HOME/.machine)
+echo "Machine: $MACHINE"
+
+# if MACHINE is 'x64-wsl' then install wsl
+if [[ "$MACHINE" == "x64-wsl" ]]; then
+	bash $HOME/tmp/preflight_wsl.sh
+	export HOMEBREW_PREFIX="/home/linuxbrew/.linuxbrew"
+	sudo mkdir -p /home/linuxbrew/.linuxbrew
+	sudo chown -R $(whoami) /home/linuxbrew/.linuxbrew
 	echo "Installing homebrew on Linux (forced=$0)"
-	export HOMEBREW_PREFIX="$HOME/.linuxbrew"
 	bash ${HOME}/tmp/linuxbrew.sh $0
 fi
 
