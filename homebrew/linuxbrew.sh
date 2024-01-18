090c4ee0f6c6808fbe1e41a59a99bec5e45d438a
Marcos Romero Lamas <marromlam@gmail.com>
2024-01-18 16:43:33 +0100

    fix: wont trigger brew install in wsl

--- homebrew/linuxbrew.sh
+++ homebrew/linuxbrew.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-if [[ -d "$HOMEBREW_PREFIX" ]]; then
-  eval $($HOMEBREW_PREFIX/bin/brew shellenv)
+if [[ -f "$HOMEBREW_PREFIX/bin/brew" ]]; then
+	eval $($HOMEBREW_PREFIX/bin/brew shellenv)
 else
-  git clone https://github.com/Homebrew/brew $HOMEBREW_PREFIX/Homebrew
-  mkdir $HOMEBREW_PREFIX/bin
-  ln -s $HOMEBREW_PREFIX/Homebrew/bin/brew $HOMEBREW_PREFIX/bin
-  eval $($HOMEBREW_PREFIX/bin/brew shellenv)
+	git clone https://github.com/Homebrew/brew $HOMEBREW_PREFIX/Homebrew
+	mkdir $HOMEBREW_PREFIX/bin
+	ln -s $HOMEBREW_PREFIX/Homebrew/bin/brew $HOMEBREW_PREFIX/bin
+	eval $($HOMEBREW_PREFIX/bin/brew shellenv)
 fi
 
 brew install stow
