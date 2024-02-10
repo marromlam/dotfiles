92e4f7d34f0aec8636e61387db8eca7627f64ed2
Marcos Romero Lamas <marromlam@gmail.com>
2024-02-10 18:21:30 +0100

    feat: add some extra deps

--- extra/ccls_patch.sh
+++
@@ -1,7 +0,0 @@
-# Fixes the following error:
-#    [coc.nvim] Unable to load global extension at 
-#    ~/fictional-couscous/files/.config/coc/extensions/node_modules/coc-ccls:
-#    main file ./lib/extension.js  not found, you may need to build the project.
-
-cd ~/.config/coc/extensions/node_modules/coc-ccls
-ln -s node_modules/ws/lib lib
