dda92a7d8bcea56ad4dd01344ef709042ab43395
Marcos Romero Lamas <marromlam@gmail.com>
2024-06-10 18:44:16 +0200

    feat: remove lazy-nvim versioning

--- files/.config/nvim/filetype.lua
+++ files/.config/nvim/filetype.lua
@@ -1,26 +1,24 @@
-if not vim.filetype then
-  return
-end
+if not vim.filetype then return end
 
 vim.filetype.add({
   extension = {
-    lock = "yaml",
+    lock = 'yaml',
   },
   filename = {
-    ["NEOGIT_COMMIT_EDITMSG"] = "NeogitCommitMessage",
-    [".psqlrc"] = "conf",
-    ["launch.json"] = "jsonc",
-    Podfile = "ruby",
-    Brewfile = "ruby",
-    Snakefile = "snakemake",
+    ['NEOGIT_COMMIT_EDITMSG'] = 'NeogitCommitMessage',
+    ['.psqlrc'] = 'conf',
+    ['launch.json'] = 'jsonc',
+    Podfile = 'ruby',
+    Brewfile = 'ruby',
+    Snakefile = 'snakemake',
   },
   pattern = {
-    [".*%.map"] = "xml",
-    [".*%.cnk"] = "xml",
-    [".*%.avsc"] = "json",
-    [".*%.conf"] = "conf",
-    [".*%.theme"] = "conf",
-    [".*%.gradle"] = "groovy",
-    ["^.env%..*"] = "bash",
+    ['.*%.map'] = 'xml', -- interchange
+    ['.*%.cnk'] = 'xml', -- interchange
+    ['.*%.avsc'] = 'json', -- avro schema
+    ['.*%.conf'] = 'conf',
+    ['.*%.theme'] = 'conf',
+    ['.*%.gradle'] = 'groovy',
+    ['^.env%..*'] = 'bash',
   },
 })
