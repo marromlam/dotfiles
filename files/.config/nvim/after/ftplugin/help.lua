e5ebe2f69bb57ef2c85993a70952271887204540
Marcos Romero Lamas <marromlam@gmail.com>
2024-06-10 18:29:21 +0200

    feat: add filetype for qf and help

--- files/.config/nvim/after/ftplugin/help.lua
+++ files/.config/nvim/after/ftplugin/help.lua
@@ -1,9 +1,12 @@
-local opt, fn, api = vim.opt_local, vim.fn, vim.api
+-- CUSTOM HELP WINDOW
+--
+-- References:
+-- https://vim.fandom.com/wiki/Learn_to_use_help
 
-opt.list = false
-opt.wrap = false
-opt.spell = true
-opt.textwidth = 78
+vim.opt_local.list = false
+vim.opt_local.wrap = false
+vim.opt_local.spell = true
+vim.opt_local.textwidth = 78
 
 -- mrl.ftplugin_conf({
 --   ["virt-column"] = function(col)
@@ -15,16 +18,18 @@ opt.textwidth = 78
 
 local opts = { buffer = 0 }
 
--- if this a vim help file create mappings to make navigation easier otherwise enable preferred editing settings
-if vim.startswith(fn.expand("%"), vim.env.VIMRUNTIME) or vim.bo.readonly then
-    opt.spell = false
-    api.nvim_create_autocmd(
-        "BufWinEnter",
-        { buffer = 0, command = "wincmd L | vertical resize 80" }
-    )
-    -- https://vim.fandom.com/wiki/Learn_to_use_help
-    map("n", "<CR>", "<C-]>", opts)
-    map("n", "<BS>", "<C-T>", opts)
+-- if this a vim help file create mappings to make navigation easier otherwise
+-- enable preferred editing settings
+if
+  vim.startswith(vim.fn.expand('%'), vim.env.VIMRUNTIME) or vim.bo.readonly
+then
+  vim.opt_local.spell = false
+  vim.api.nvim_create_autocmd(
+    'BufWinEnter',
+    { buffer = 0, command = 'wincmd L | vertical resize 80' }
+  )
+  map('n', '<CR>', '<C-]>', opts)
+  map('n', '<BS>', '<C-T>', opts)
 else
-    map("n", "<leader>ml", "maGovim:tw=78:ts=8:noet:ft=help:norl:<esc>`a", opts)
+  map('n', '<leader>ml', 'maGovim:tw=78:ts=8:noet:ft=help:norl:<esc>`a', opts)
 end
--- files/.config/nvim/after/ftplugin/qf.lua
+++ files/.config/nvim/after/ftplugin/qf.lua
@@ -1,27 +1,30 @@
-local opt = vim.opt_local
+-- CUSTOM QUICKFIX WINDOW
+--
+-- References:
+-- https://vim.fandom.com/wiki/Learn_to_use_help
 
-opt.wrap = false
-opt.number = false
-opt.signcolumn = "yes"
-opt.buflisted = false
-opt.winfixheight = true
+vim.opt_local.wrap = false
+vim.opt_local.number = false
+vim.opt_local.signcolumn = 'yes'
+vim.opt_local.buflisted = false
+vim.opt_local.winfixheight = true
 
 map(
-    "n",
-    "dd",
-    mrl.list.qf.delete,
-    { desc = "delete current quickfix entry", buffer = 0 }
+  'n',
+  'dd',
+  mrl.list.qf.delete,
+  { desc = '[qf] delete current quickfix entry', buffer = 0 }
 )
 map(
-    "v",
-    "d",
-    mrl.list.qf.delete,
-    { desc = "delete selected quickfix entry", buffer = 0 }
+  'v',
+  'd',
+  mrl.list.qf.delete,
+  { desc = '[qf] delete selected quickfix entry', buffer = 0 }
 )
 
-map("n", "H", ":colder<CR>", { buffer = 0 })
-map("n", "L", ":cnewer<CR>", { buffer = 0 })
+map('n', 'H', ':colder<CR>', { buffer = 0 })
+map('n', 'L', ':cnewer<CR>', { buffer = 0 })
 
 -- force quickfix to open beneath all other splits
-vim.cmd.wincmd("J")
+vim.cmd.wincmd('J')
 mrl.adjust_split_height(3, 10)
