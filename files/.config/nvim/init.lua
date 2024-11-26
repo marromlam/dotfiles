7723bf34282c71937f7573c5423d90f79e6006aa
Marcos Romero Lamas <marromlam@gmail.com>
2024-11-26 11:40:37 +0100

    feat: new init

--- files/.config/nvim/init.lua
+++ files/.config/nvim/init.lua
@@ -14,8 +14,9 @@ vim.g.icloud = vim.fn.expand('~') .. '/Library/Mobile Documents'
 vim.g.obsidian = vim.g.icloud .. '/iCloud~md~obsidian/Documents'
 
 -- Leader bindings
-vim.g.mapleader = ' ' -- Remap leader key
+vim.g.mapleader = ' '       -- Remap leader key
 vim.g.maplocalleader = '\\' -- Local leader is <Space>
+
 
 --------------------------------------------------------------------------------
 -- Global namespace {{{
@@ -38,22 +39,29 @@ _G.mrl = mrl or namespace
 _G.map = vim.keymap.set
 _G.P = vim.print
 
--- }}}
-
--- If opening from inside neovim terminal, skip {{{
+-- If opening from inside neovim terminal, skip
 if vim.env.NVIM then
   return require('lazy').setup({ { 'willothy/flatten.nvim', config = true } })
 end
+
 -- }}}
 --------------------------------------------------------------------------------
 
--- Load modules
+
+--------------------------------------------------------------------------------
+-- Load modules {{{
+--------------------------------------------------------------------------------
+
 require('tools') -- has to be loaded before plugins
 -- require('remaps')
 require('keymaps')
-require('autocommands')
+require('tools')
+-- require('autocommands')
 require('custom.globals')
 require('options')
+require('highlight')
+require('custom.ui')
+require('custom.strings')
 require('lazyloader')
 require('custom.ui')
 require('external_grep')
@@ -64,202 +72,37 @@ vim.keymap.set(
   '<cmd>lua vim.lsp.diagnostic.get_line_diagnostics()<CR>',
   { noremap = true, silent = true }
 )
--- local ns = require('lint').get_namespace('my_linter_name')
--- vim.diagnostic.config({ virtual_text = true }, ns)
-
---------------------------------------------------------------------------------
--- Extra hightlighting {{{
---------------------------------------------------------------------------------
-
-function mrl.get_hi(name, id)
-  id = id or 0
-  local hi = vim.api.nvim_get_hl(0, { name = name })
-  -- hi is a table with bg and fg keys. for those we want to return the hex
-  -- value with ('#%06x'):format(num)
-  for k, v in pairs(hi) do
-    if type(v) == 'number' then hi[k] = ('#%06x'):format(v) end
-  end
-  return hi
-end
-
--- vim.api.nvim_create_user_command("Format", function()
---   -- check if null-ls exists
---   local check, nullls = pcall(require, "null-ls")
---   -- check if a formatting source of null-ls is registered
---   --  wait 2 sec to get the server started
---   vim.wait(2000, function()
---     return check
---       and nullls.is_registered({ method = nullls.methods.FORMATTING })
---   end, 1)
---
---   if check and nullls.is_registered({ method = nullls.methods.FORMATTING }) then
---     vim.lsp.buf.format()
---   else
---     vim.cmd([[normal gg=G<C-o>]])
---   end
--- end, {})
---
--- vim.keymap.set(
---   { "n" },
---   "<leader>lf",
---   "<cmd>Format<CR>",
---   { silent = true, desc = "lsp-format current buffer" }
--- )
-
-gruvbox = require('gruvbox')
-
-vim.api.nvim_set_hl(0, 'LineNr', {
-  fg = mrl.get_hi('Comment').fg,
-  bg = mrl.get_hi('Normal').bg,
-})
-vim.api.nvim_set_hl(0, 'LineNrAbove', {
-  fg = mrl.get_hi('Comment').fg,
-  bg = mrl.get_hi('Normal').bg,
-})
-vim.api.nvim_set_hl(0, 'NotifyBackground', {
-  fg = mrl.get_hi('Comment').fg,
-  bg = mrl.get_hi('Normal').bg,
-})
-vim.api.nvim_set_hl(0, 'LineNrBelow', {
-  fg = mrl.get_hi('Comment').fg,
-  bg = mrl.get_hi('Normal').bg,
-})
-
--- vim.api.nvim_set_hl(0, 'GitSignsAdd', {
---   fg = mrl.get_hi('GitSignsAdd').fg,
---   bg = mrl.get_hi('Normal').bg,
--- })
--- vim.api.nvim_set_hl(0, 'GitSignsChange', {
---   fg = mrl.get_hi('GitSignsChange').fg,
---   bg = mrl.get_hi('Normal').bg,
--- })
--- vim.api.nvim_set_hl(0, 'GitSignsDelete', {
---   fg = mrl.get_hi('GitSignsDelete').fg,
---   bg = mrl.get_hi('Normal').bg,
--- })
--- vim.api.nvim_set_hl(0, 'GitSignsUntracked', {
---   fg = mrl.get_hi('GitSignsUntracked').fg,
---   bg = mrl.get_hi('Normal').bg,
--- })
-
--- general color for text in status line
-vim.api.nvim_set_hl(0, 'Statusline', {
-  -- fg = mrl.get_hi('Statusline').fg,
-  fg = '#00ff00',
-  bg = '#213352',
-  -- bg = mrl.get_hi('Normal').bg,
-})
-vim.api.nvim_set_hl(0, 'CustomStatusline', {
-  -- fg = mrl.get_hi('Statusline').fg,
-  fg = '#00ff00',
-  bg = '#213352',
-  -- bg = mrl.get_hi('Normal').bg,
-})
-vim.api.nvim_set_hl(0, 'StatusLine', {
-  -- fg = mrl.get_hi('Statusline').fg,
-  fg = '#00ff00',
-  bg = '#213352',
-  -- bg = mrl.get_hi('Normal').bg,
-})
-vim.api.nvim_set_hl(0, 'StCustomError', {
-  -- fg = mrl.get_hi('Statusline').fg,
-  fg = '#ff0000',
-  bg = '#213352',
-  -- bg = mrl.get_hi('Normal').bg,
-})
-
-vim.api.nvim_set_hl(0, 'StTitle', {
-  -- fg = mrl.get_hi('Statusline').fg,
-  fg = '#00ff00',
-  bg = '#213352',
-  -- bg = mrl.get_hi('Normal').bg,
-})
-
-vim.api.nvim_set_hl(0, 'StatuslineGitSignsAdd', {
-  -- fg = mrl.get_hi('DiffAdd').fg,
-  fg = '#00ffaa',
-  bg = mrl.get_hi('Statusline').bg,
-})
-
-vim.api.nvim_set_hl(0, 'StatuslineGitSignsDelete', {
-  -- fg = mrl.get_hi('WarningMsg').fg,
-  fg = '#ff0000',
-  bg = mrl.get_hi('StatusLine').bg,
-})
-
-vim.api.nvim_set_hl(0, 'StatuslineSearch', {
-  fg = mrl.get_hi('Search').fg,
-  bg = mrl.get_hi('Search').bg,
-})
-
-vim.api.nvim_set_hl(0, 'StatuslineRed', {
-  fg = mrl.get_hi('GitSignsDelete').fg,
-  bg = mrl.get_hi('Statusline').bg,
-})
-
-vim.api.nvim_set_hl(0, 'StBranch', {
-  fg = '#ffaa00',
-  -- bg = mrl.get_hi('GruvboxRedSign').fg,
-})
-
-vim.api.nvim_set_hl(0, 'StBranch', {
-  fg = mrl.get_hi('StatuslineRed').fg,
-  -- bg = mrl.get_hi('GruvboxRedSign').fg,
-})
-
-vim.api.nvim_set_hl(0, 'StatuslineParentDirectory', {
-  fg = mrl.get_hi('IncSearch').bg,
-  bg = mrl.get_hi('Statusline').bg,
-  bold = true,
-})
-vim.api.nvim_set_hl(0, 'StatuslineEnv', {
-  fg = mrl.get_hi('GitSignsDelete').fg,
-  bg = mrl.get_hi('Statusline').bg,
-  bold = true,
-  italic = true,
-})
-vim.api.nvim_set_hl(0, 'StatuslineDirectory', {
-  fg = mrl.get_hi('Comment').fg,
-  bg = mrl.get_hi('Statusline').bg,
-  bold = false,
-  italic = true,
-})
-vim.api.nvim_set_hl(0, 'StatuslineDirectoryInactive', {
-  fg = mrl.get_hi('Comment').fg,
-  bg = mrl.get_hi('Statusline').bg,
-  bold = false,
-  italic = false,
-})
-vim.api.nvim_set_hl(0, 'StatuslineFilename', {
-  fg = mrl.get_hi('Normal').fg,
-  bg = mrl.get_hi('Statusline').bg,
-  bold = true,
-  italic = false,
-})
-vim.api.nvim_set_hl(0, 'WinSeparator', {
-  fg = mrl.get_hi('Statusline').fg,
-  bg = mrl.get_hi('Normal').bg,
-  bold = false,
-})
 
 -- }}}
 --------------------------------------------------------------------------------
 
+
+--------------------------------------------------------------------------------
+-- Colorscheme and extra temporal highlights {{{
+--------------------------------------------------------------------------------
+
 vim.o.background = 'dark' -- or "light"
 vim.cmd([[colorscheme gruvbox]])
+-- vim.cmd([[colorscheme horizon]])
+-- vim.cmd([[colorscheme rose-pine]])
+-- vim.cmd([[colorscheme github_dark]])
 
 vim.cmd([[
 
 hi Match ctermbg=162
 sig define highlightline linehl=Match
 au TextChanged,TextChangedI,TextChangedP,BufWinEnter,BufWritePost,FileWritePost * if expand("%:p") != "" | exe("call map(range(1,1000), {i->execute('sig unplace 999 file='.expand('%:p'))})") | call map(getline(1, '$'), {idx, val -> execute('if val =~ "^\\s*##" | exe "sig place 999 line=".expand(idx+1)." name=highlightline file=".expand("%:p") | endif')}) | endif
-
-
 ]])
+
 
 vim.cmd([[
 autocmd! FileType fzf tnoremap <buffer> <esc> <c-c>
 autocmd! FileType fzf tnoremap <buffer> <esc><esc> <c-c>
 ]])
+
+
+-- }}}
+--------------------------------------------------------------------------------
+
 
 -- vim: ts=2 sts=2 sw=2 et fdm=marker
---
+++ files/.config/nvim/plugin/autocommands.lua
@@ -0,0 +1,174 @@
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
+--
+--
+if not mrl then return end
+
+local fn, api, v, env, cmd, fmt = vim.fn, vim.api, vim.v, vim.env, vim.cmd, string.format
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup(
+    'kickstart-highlight-yank',
+    { clear = true }
+  ),
+  callback = function() vim.highlight.on_yank() end,
+})
+
+function mrl.augroup(name, ...)
+  local commands = { ... }
+  assert(name ~= 'User', 'The name of an augroup CANNOT be User')
+  assert(
+    #commands > 0,
+    fmt('You must specify at least one autocommand for %s', name)
+  )
+  local id = api.nvim_create_augroup(name, { clear = true })
+  for _, autocmd in ipairs(commands) do
+    -- validate_autocmd(name, autocmd)
+    local is_callback = type(autocmd.command) == 'function'
+    vim.api.nvim_create_autocmd(autocmd.event, {
+      group = name,
+      pattern = autocmd.pattern,
+      desc = autocmd.desc,
+      callback = is_callback and autocmd.command or nil,
+      command = not is_callback and autocmd.command or nil,
+      once = autocmd.once,
+      nested = autocmd.nested,
+      buffer = autocmd.buffer,
+    })
+  end
+  return id
+end
+
+local function stop_hl()
+  if v.hlsearch == 0 or api.nvim_get_mode().mode ~= 'n' then return end
+  api.nvim_feedkeys(vim.keycode('<Plug>(StopHL)'), 'm', false)
+end
+
+local function hl_search()
+  local col = api.nvim_win_get_cursor(0)[2]
+  local curr_line = api.nvim_get_current_line()
+  local ok, match = pcall(fn.matchstrpos, curr_line, fn.getreg('/'), 0)
+  if not ok then return end
+  local _, p_start, p_end = unpack(match)
+  -- if the cursor is in a search result, leave highlighting on
+  if col < p_start or col > p_end then stop_hl() end
+end
+
+mrl.augroup('VimrcIncSearchHighlight', {
+  event = { 'CursorMoved' },
+  command = function() hl_search() end,
+}, {
+  event = { 'InsertEnter' },
+  command = function() stop_hl() end,
+}, {
+  event = { 'OptionSet' },
+  pattern = { 'hlsearch' },
+  command = function()
+    vim.schedule(function() cmd.redrawstatus() end)
+  end,
+}, {
+  event = 'RecordingEnter',
+  command = function() vim.o.hlsearch = false end,
+}, {
+  event = 'RecordingLeave',
+  command = function() vim.o.hlsearch = true end,
+})
+
+-- Search highlighting {{{
+
+----------------------------------------------------------------------------------------------------
+-- HLSEARCH
+----------------------------------------------------------------------------------------------------
+-- In order to get hlsearch working the way I like i.e. on when using /,?,N,n,*,#, etc. and off when
+-- When I'm not using them, I need to set the following:
+-- The mappings below are essentially faked user input this is because in order to automatically turn off
+-- the search highlight just changing the value of 'hlsearch' inside a function does not work
+-- read `:h nohlsearch`. So to have this workaround I check that the current mouse position is not a search
+-- result, if it is we leave highlighting on, otherwise I turn it off on cursor moved by faking my input
+-- using the expr mappings below.
+--
+-- This is based on the implementation discussed here:
+-- https://github.com/neovim/neovim/issues/5581
+
+
+vim.keymap.set( { 'n', 'v', 'o', 'i', 'c' }, '<Plug>(StopHL)', 'execute("nohlsearch")[-1]', { expr = true })
+
+local function stop_hl()
+  if v.hlsearch == 0 or api.nvim_get_mode().mode ~= 'n' then return end
+  api.nvim_feedkeys(vim.keycode('<Plug>(StopHL)'), 'm', false)
+end
+
+local function hl_search()
+  local col = api.nvim_win_get_cursor(0)[2]
+  local curr_line = api.nvim_get_current_line()
+  local ok, match = pcall(fn.matchstrpos, curr_line, fn.getreg('/'), 0)
+  if not ok then return end
+  local _, p_start, p_end = unpack(match)
+  -- if the cursor is in a search result, leave highlighting on
+  if col < p_start or col > p_end then stop_hl() end
+end
+
+mrl.augroup('VimrcIncSearchHighlight', {
+  event = { 'CursorMoved' },
+  command = function() hl_search() end,
+}, {
+  event = { 'InsertEnter' },
+  command = function() stop_hl() end,
+}, {
+  event = { 'OptionSet' },
+  pattern = { 'hlsearch' },
+  command = function()
+    vim.schedule(function() cmd.redrawstatus() end)
+  end,
+}, {
+  event = 'RecordingEnter',
+  command = function() vim.o.hlsearch = false end,
+}, {
+  event = 'RecordingLeave',
+  command = function() vim.o.hlsearch = true end,
+})
+
+-- }}}
+
+
+
+-- Recording macro {{{
+
+vim.api.nvim_create_autocmd('RecordingEnter', {
+  pattern = '*',
+  callback = function()
+    vim.g.macro_recording = 'macro @' .. vim.fn.reg_recording()
+    vim.cmd('redrawstatus')
+  end,
+})
+
+-- Autocmd to track the end of macro recording
+vim.api.nvim_create_autocmd('RecordingLeave', {
+  pattern = '*',
+  callback = function()
+    vim.g.macro_recording = ''
+    vim.cmd('redrawstatus')
+  end,
+})
+
+-- }}}
+
+
+
+
+mrl.augroup('UpdateVim', {
+  event = { 'FocusLost' },
+  pattern = { '*' },
+  command = 'silent! wall',
+}, {
+  event = { 'VimResized' },
+  pattern = { '*' },
+  command = 'wincmd =', -- Make windows equal size when vim resizes
+})
+
+
+-- }}}
