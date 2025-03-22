fc327fcb71365d04242a8666cd5ed27b35102130
Marcos Romero Lamas <marromlam@gmail.com>
2025-03-22 16:34:46 +0100

    feat: add keyanalyzer

--- files/.config/nvim/lua/custom/plugins/init.lua
+++ files/.config/nvim/lua/custom/plugins/init.lua
@@ -1,32 +1,3 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
-function mrl.augroup(name, ...)
-  local commands = { ... }
-  assert(name ~= 'User', 'The name of an augroup CANNOT be User')
-  assert(
-    #commands > 0,
-    string.format('You must specify at least one autocommand for %s', name)
-  )
-  local id = vim.api.nvim_create_augroup(name, { clear = true })
-  for _, autocmd in ipairs(commands) do
-    --  validate_autocmd(name, autocmd)
-    local is_callback = type(autocmd.command) == 'function'
-    vim.api.nvim_create_autocmd(autocmd.event, {
-      group = name,
-      pattern = autocmd.pattern,
-      desc = autocmd.desc,
-      callback = is_callback and autocmd.command or nil,
-      command = not is_callback and autocmd.command or nil,
-      once = autocmd.once,
-      nested = autocmd.nested,
-      buffer = autocmd.buffer,
-    })
-  end
-  return id
-end
-
 return {
   {
     'jghauser/fold-cycle.nvim',
@@ -224,9 +195,8 @@ return {
       },
     },
   },
+  { 'meznaric/key-analyzer.nvim', opts = {}, cmd = 'KeyAnalyzer' },
 
   --- }}}
   ------------------------------------------------------------------------
 }
-
--- vim:foldmethod=marker
