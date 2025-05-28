0c1601f87b23c94f5336e53ff93236e4dfa6a8cf
Marcos Romero Lamas <marromlam@gmail.com>
2025-05-29 01:40:02 +0200

    feat: add sonarqube

--- files/.config/nvim/lua/custom/plugins/format.lua
+++ files/.config/nvim/lua/custom/plugins/format.lua
@@ -9,6 +9,7 @@ return {
         '<leader>lf',
         function()
           require('conform').format({ async = true, lsp_fallback = true })
+          local a = 2
         end,
         mode = '',
         desc = '[F]ormat buffer',
@@ -17,85 +18,83 @@ return {
     config = function()
       local conform = require('conform')
       conform.setup({
-      notify_on_error = true,
-      format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        if vim.g.disable_autoformat then return false end
-        local disable_filetypes =
-          { c = true, cpp = true, xml = true, cnk = true, map = true }
-        return {
-          timeout_ms = 5000,
-          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-        }
-      end,
-      formatters_by_ft = {
-        bash = { 'shfmt' },
-        javascript = { 'prettier' },
-        typescript = { 'prettier' },
-        javascriptreact = { 'prettier' },
-        typescriptreact = { 'prettier' },
-        svelte = { 'prettier' },
-        css = { 'prettierd' },
-        html = { 'prettier' },
-        json = { 'prettier' },
-        yaml = { 'prettier' },
-        markdown = { 'prettier' },
-        graphql = { 'prettier' },
-        liquid = { 'prettier' },
-        lua = { 'stylua' },
-        python = { 'isort', 'black' },
-        xml = { 'xmlformat' },
-      },
-      -- Set default options
-      default_format_opts = {
-        lsp_format = 'fallback',
-      },
-      -- Set up format-on-save
-      -- format_on_save = { timeout_ms = 500 },
-      -- Customize formatters
-      formatters = {
-        shfmt = {
-          prepend_args = { '-i', '2' },
+        notify_on_error = true,
+        format_on_save = function(bufnr)
+          -- Disable "format_on_save lsp_fallback" for languages that don't
+          -- have a well standardized coding style. You can add additional
+          -- languages here or re-enable it for the disabled ones.
+          if vim.g.disable_autoformat then return nil end
+          local disable_filetypes =
+            { c = true, cpp = true, xml = true, cnk = true, map = true }
+          return {
+            timeout_ms = 5000,
+            lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+          }
+        end,
+        formatters_by_ft = {
+          bash = { 'shfmt' },
+          javascript = { 'prettier' },
+          typescript = { 'prettier' },
+          javascriptreact = { 'prettier' },
+          typescriptreact = { 'prettier' },
+          svelte = { 'prettier' },
+          css = { 'prettierd' },
+          html = { 'prettier' },
+          json = { 'prettier' },
+          yaml = { 'prettier' },
+          markdown = { 'prettier' },
+          graphql = { 'prettier' },
+          liquid = { 'prettier' },
+          lua = { 'stylua' },
+          python = { 'isort', 'black' },
+          xml = { 'xmlformat' },
         },
-      }
-      }
-  )
+        -- Set default options
+        default_format_opts = {
+          lsp_format = 'fallback',
+        },
+        -- Set up format-on-save
+        -- format_on_save = { timeout_ms = 500 },
+        -- Customize formatters
+        formatters = {
+          shfmt = {
+            prepend_args = { '-i', '2' },
+          },
+        },
+      })
 
-    vim.api.nvim_create_user_command('Format', function(args)
-    local range = nil
-    if args.count ~= -1 then
-        local end_line =
-        vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
-        range = {
-        start = { args.line1, 0 },
-        ['end'] = { args.line2, end_line:len() },
+      vim.api.nvim_create_user_command('Format', function(args)
+        local range = nil
+        if args.count ~= -1 then
+          local end_line =
+            vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+          range = {
+            start = { args.line1, 0 },
+            ['end'] = { args.line2, end_line:len() },
+          }
+        end
+        conform.format({
+          async = true,
+          lsp_format = 'fallback',
+          range = range,
+        })
+      end, { range = true })
+
+      vim.api.nvim_create_user_command(
+        'FormatDisable',
+        function(args) vim.g.disable_autoformat = true end,
+        {
+          desc = 'Disable autoformat-on-save',
         }
-    end
-    conform.format({
-        async = true,
-        lsp_format = 'fallback',
-        range = range,
-    })
-    end, { range = true })
+      )
 
-    vim.api.nvim_create_user_command(
-    'FormatDisable',
-    function(args) vim.g.disable_autoformat = true end,
-    {
-        desc = 'Disable autoformat-on-save',
-    }
-    )
-
-    vim.api.nvim_create_user_command(
-    'FormatEnable',
-    function() vim.g.disable_autoformat = false end,
-    {
-        desc = 'Re-enable autoformat-on-save',
-    }
-    )
-
-  end
-  }
+      vim.api.nvim_create_user_command(
+        'FormatEnable',
+        function() vim.g.disable_autoformat = false end,
+        {
+          desc = 'Re-enable autoformat-on-save',
+        }
+      )
+    end,
+  },
 }
--- files/.config/nvim/lua/custom/plugins/lsp.lua
+++ files/.config/nvim/lua/custom/plugins/lsp.lua
@@ -419,84 +419,113 @@ return { -- LSP Configuration & Plugins
       })
     end,
   },
-}
 
--- return {
---   {
---     "neovim/nvim-lspconfig",
---     event = "VeryLazy",
---     init = function()
---       local keys = require("lazyvim.plugins.lsp.keymaps").get()
---       -- change a keymap
---       keys[#keys + 1] = { "gh", "<cmd>lua vim.lsp.buf.hover()<cr>" }
---       -- disable a keymap
---       keys[#keys + 1] = { "gD", false }
---       keys[#keys + 1] = { "gK", false }
---     end,
---   },
---   {
---     "DNLHC/glance.nvim",
---     opts = {
---       preview_win_opts = { relativenumber = false },
---       theme = { enable = true },
---     },
---     keys = {
---       { "gD", "<Cmd>Glance definitions<CR>", desc = "lsp: glance definitions" },
---       { "gR", "<Cmd>Glance references<CR>", desc = "lsp: glance references" },
---       {
---         "gY",
---         "<Cmd>Glance type_definitions<CR>",
---         desc = "lsp: glance type definitions",
---       },
---       {
---         "gM",
---         "<Cmd>Glance implementations<CR>",
---         desc = "lsp: glance implementations",
---       },
---     },
---   },
---   {
---     "smjonas/inc-rename.nvim",
---     opts = { hl_group = "Visual", preview_empty_name = true },
---     keys = {
---       {
---         "<leader>rn",
---         function()
---           return vim.fmt(":IncRename %s", vim.fn.expand("<cword>"))
---         end,
---         expr = true,
---         silent = false,
---         desc = "lsp: incremental rename",
---       },
---     },
---   },
---   {
---     "lvimuser/lsp-inlayhints.nvim",
---     init = function()
---       mrl.augroup("InlayHintsSetup", {
---         event = "LspAttach",
---         command = function(args)
---           local id = vim.tbl_get(args, "data", "client_id") --[[@as lsp.Client]]
---           if not id then
---             return
---           end
---           local client = vim.lsp.get_client_by_id(id)
---           require("lsp-inlayhints").on_attach(client, args.buf)
---         end,
---       })
---     end,
---     opts = {
---       inlay_hints = {
---         highlight = "Comment",
---         labels_separator = " ⏐ ",
---         parameter_hints = { prefix = "󰊕" },
---         type_hints = { prefix = "=> ", remove_colon_start = true },
---       },
---     },
---   },
---   {
---     "simrat39/rust-tools.nvim",
---     dependencies = { "nvim-lspconfig" },
---     ft = "rust",
---   },
--- }
+  {
+    url = 'https://gitlab.com/schrieveslaach/sonarlint.nvim',
+    ft = { 'python', 'cpp', 'java' },
+    event = 'LspAttach',
+    dependencies = {
+      'williamboman/mason.nvim',
+    },
+    config = function()
+      local mason_path = os.getenv('MASON')
+      require('sonarlint').setup({
+        server = {
+          cmd = {
+            'sonarlint-language-server',
+            -- Ensure that sonarlint-language-server uses stdio channel
+            '-stdio',
+            '-analyzers',
+            -- paths to the analyzers you need, using those for python and java in this example
+            vim.fn.expand('$MASON/share/sonarlint-analyzers/sonarpython.jar'),
+            vim.fn.expand('$MASON/share/sonarlint-analyzers/sonarcfamily.jar'),
+            vim.fn.expand('$MASON/share/sonarlint-analyzers/sonarjava.jar'),
+          },
+        },
+        filetypes = {
+          'python',
+        },
+      })
+    end,
+  },
+
+  -- return {
+  --   {
+  --     "neovim/nvim-lspconfig",
+  --     event = "VeryLazy",
+  --     init = function()
+  --       local keys = require("lazyvim.plugins.lsp.keymaps").get()
+  --       -- change a keymap
+  --       keys[#keys + 1] = { "gh", "<cmd>lua vim.lsp.buf.hover()<cr>" }
+  --       -- disable a keymap
+  --       keys[#keys + 1] = { "gD", false }
+  --       keys[#keys + 1] = { "gK", false }
+  --     end,
+  --   },
+  --   {
+  --     "DNLHC/glance.nvim",
+  --     opts = {
+  --       preview_win_opts = { relativenumber = false },
+  --       theme = { enable = true },
+  --     },
+  --     keys = {
+  --       { "gD", "<Cmd>Glance definitions<CR>", desc = "lsp: glance definitions" },
+  --       { "gR", "<Cmd>Glance references<CR>", desc = "lsp: glance references" },
+  --       {
+  --         "gY",
+  --         "<Cmd>Glance type_definitions<CR>",
+  --         desc = "lsp: glance type definitions",
+  --       },
+  --       {
+  --         "gM",
+  --         "<Cmd>Glance implementations<CR>",
+  --         desc = "lsp: glance implementations",
+  --       },
+  --     },
+  --   },
+  --   {
+  --     "smjonas/inc-rename.nvim",
+  --     opts = { hl_group = "Visual", preview_empty_name = true },
+  --     keys = {
+  --       {
+  --         "<leader>rn",
+  --         function()
+  --           return vim.fmt(":IncRename %s", vim.fn.expand("<cword>"))
+  --         end,
+  --         expr = true,
+  --         silent = false,
+  --         desc = "lsp: incremental rename",
+  --       },
+  --     },
+  --   },
+  --   {
+  --     "lvimuser/lsp-inlayhints.nvim",
+  --     init = function()
+  --       mrl.augroup("InlayHintsSetup", {
+  --         event = "LspAttach",
+  --         command = function(args)
+  --           local id = vim.tbl_get(args, "data", "client_id") --[[@as lsp.Client]]
+  --           if not id then
+  --             return
+  --           end
+  --           local client = vim.lsp.get_client_by_id(id)
+  --           require("lsp-inlayhints").on_attach(client, args.buf)
+  --         end,
+  --       })
+  --     end,
+  --     opts = {
+  --       inlay_hints = {
+  --         highlight = "Comment",
+  --         labels_separator = " ⏐ ",
+  --         parameter_hints = { prefix = "󰊕" },
+  --         type_hints = { prefix = "=> ", remove_colon_start = true },
+  --       },
+  --     },
+  --   },
+  --   {
+  --     "simrat39/rust-tools.nvim",
+  --     dependencies = { "nvim-lspconfig" },
+  --     ft = "rust",
+  --   },
+  -- }
+}
