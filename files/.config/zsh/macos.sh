fcb20a265475bf8b42a21b1f2d448c03742426cf
Marcos Romero Lamas <marromlam@gmail.com>
2026-02-18 00:50:53 +0100

    feat: some improvements to tmux theme

--- files/.config/zsh/macos.sh
+++ files/.config/zsh/macos.sh
@@ -2,3 +2,8 @@
 export BASH_SILENCE_DEPRECATION_WARNING=1
 
 ulimit -S -n 2048
+
+# Add OpenJDK to PATH (required for Neovim SonarLint plugin)
+if [[ -d "/opt/homebrew/opt/openjdk/bin" ]]; then
+  export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"
+fi
