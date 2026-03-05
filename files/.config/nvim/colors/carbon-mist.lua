3650b8e84c0ef08e1a4ddcd31c1aa474e64bb7b2
Marcos Romero Lamas <marromlam@gmail.com>
2026-03-05 15:32:31 +0100

    feat: add ghostty config

---
+++ files/.config/nvim/colors/carbon-mist.lua
@@ -0,0 +1,14 @@
+-- Carbon Mist color scheme for Neovim
+-- Carbon Mist theme based on a soft retro ANSI palette.
+-- Author: Marcos Romero Lamas
+-- Version: 1.0.0
+
+vim.cmd("hi clear")
+if vim.fn.exists("syntax_on") then
+  vim.cmd("syntax reset")
+end
+
+vim.o.termguicolors = true
+vim.g.colors_name = "carbon-mist"
+
+require("carbon-mist").load()
