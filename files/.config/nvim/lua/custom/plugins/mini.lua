7c65b1a66024acb6c8188eb27753a6a03975e85f
Marcos Romero Lamas <marromlam@gmail.com>
2026-03-13 23:31:53 +0100

    feat: update some plugin config

--- files/.config/nvim/lua/custom/plugins/mini.lua
+++ files/.config/nvim/lua/custom/plugins/mini.lua
@@ -198,37 +198,6 @@ return { -- Collection of various small independent plugins/modules
 
   -- Visual enhancements
   {
-    'echasnovski/mini.hipatterns',
-    event = { 'BufReadPre', 'BufNewFile' },
-    config = function()
-      local hipatterns = require('mini.hipatterns')
-      hipatterns.setup({
-        highlighters = {
-          -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
-          fixme = {
-            pattern = '%f[%w]()FIXME()%f[%W]',
-            group = 'MiniHipatternsFixme',
-          },
-          hack = {
-            pattern = '%f[%w]()HACK()%f[%W]',
-            group = 'MiniHipatternsHack',
-          },
-          todo = {
-            pattern = '%f[%w]()TODO()%f[%W]',
-            group = 'MiniHipatternsTodo',
-          },
-          note = {
-            pattern = '%f[%w]()NOTE()%f[%W]',
-            group = 'MiniHipatternsNote',
-          },
-
-          -- Highlight hex color strings (`#rrggbb`) using that color
-          hex_color = hipatterns.gen_highlighter.hex_color(),
-        },
-      })
-    end,
-  },
-  {
     'echasnovski/mini.indentscope',
     enabled = false,
     cond = false,
